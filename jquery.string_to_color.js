f62b14c533446f11b77627b239bf16a15ee00ee6
Mattias Erming <mattias@mattiaserming.com>
2014-06-07 16:21:51 +0200

    Initial commit

---
+++ jquery.string_to_color.js
@@ -0,0 +1,86 @@
+/*!
+ * jquery-string_to_color
+ * A jQuery plugin based on string_to_color by Brandon Corbin.
+ *
+ * Source:
+ * https://github.com/erming/jquery-string_to_color
+ *
+ * Version 0.0.0
+ */
+(function($) {
+	/**
+	 * Generate hex color code from a string.
+	 *
+	 * @param {String} str
+	 */
+	$.string_to_color = function(str) {
+		return "#" + string_to_color(str);
+	};
+	
+	/**
+	 * Set one or more CSS properties for the set of matched elements.
+	 *
+	 * @param {String|Array} property
+	 * @param {String} str
+	 */
+	$.fn.string_to_color = function(property, string) {
+		if (!property || !string) {
+			throw new Error("$(selector).string_to_color() takes 2 arguments");
+		}
+		return this.each(function() {
+			var props = [].concat(property);
+			var $this = $(this);
+			$.map(props, function(p) {
+				console.log(p);
+				$this.css(p, $.string_to_color(string));
+			});
+		});
+	};
+})(jQuery);
+ 
+/********************************************************
+Name: str_to_color
+Description: create a hash from a string then generates a color
+Usage: alert('#'+str_to_color("Any string can be converted"));
+author: Brandon Corbin [code@icorbin.com]
+website: http://icorbin.com
+********************************************************/
+
+function string_to_color(str, options) {
+
+    // Generate a Hash for the String
+    this.hash = function(word) {
+        var h = 0;
+        for (var i = 0; i < word.length; i++) {
+            h = word.charCodeAt(i) + ((h << 5) - h);
+        }
+        return h;
+    };
+
+    // Change the darkness or lightness
+    this.shade = function(color, prc) {
+        var num = parseInt(color, 16),
+            amt = Math.round(2.55 * prc),
+            R = (num >> 16) + amt,
+            G = (num >> 8 & 0x00FF) + amt,
+            B = (num & 0x0000FF) + amt;
+        return (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
+            (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 +
+            (B < 255 ? B < 1 ? 0 : B : 255))
+            .toString(16)
+            .slice(1);
+
+    };
+    
+    // Convert init to an RGBA
+    this.int_to_rgba = function(i) {
+        var color = ((i >> 24) & 0xFF).toString(16) +
+            ((i >> 16) & 0xFF).toString(16) +
+            ((i >> 8) & 0xFF).toString(16) +
+            (i & 0xFF).toString(16);
+        return color;
+    };
+
+    return this.shade(this.int_to_rgba(this.hash(str)), -10);
+
+}
